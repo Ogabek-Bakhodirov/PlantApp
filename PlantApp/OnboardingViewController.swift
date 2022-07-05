5285b7e2ca5fed69c2c5984a88e45b0b54cd0458
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-07-05 22:14:20 +0500

    Combine label stack view

--- PlantApp/OnboardingViewController.swift
+++ PlantApp/OnboardingViewController.swift
@@ -18,6 +18,67 @@ class OnboardingViewController: UIViewController {
         return view
     }()
 
+    lazy var titleLabel: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.text = "Identify Plants"
+        view.font = .systemFont(ofSize: 19.0, weight: .bold)
+        view.textColor = UIColor(named: "main_title_color")
+        view.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        view.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+        view.textAlignment = .center
+
+        return view
+    }()
+
+    lazy var descriptionLabel: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.text = "You can identify the plants you don't know through your camera"
+        view.numberOfLines = 3
+        view.minimumScaleFactor = 0.7
+        view.contentMode = .top
+        view.textAlignment = .center
+        view.font = .systemFont(ofSize: 13.0, weight: .regular)
+        view.textColor = UIColor(named: "main_title_color")
+        view.setContentHuggingPriority(.defaultLow, for: .vertical)
+        view.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+
+        return view
+    }()
+
+    lazy var labelStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [titleLabel, descriptionLabel])
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
+        view.distribution = .fill
+        view.contentMode = .center
+        view.spacing = 10
+
+        return view
+    }()
+
+    lazy var dotImageView: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.contentMode = .scaleAspectFit
+        view.image = UIImage(named: "ic_dots_1")
+        view.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        view.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+
+        return view
+    }()
+
+    lazy var contentStack: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [mainImageView, labelStackView, dotImageView])
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
+        view.distribution = .fill
+        view.contentMode = .center
+        view.spacing = 40
+
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,13 +93,17 @@ class OnboardingViewController: UIViewController {
     }
 
     private func setupSubviews() {
-        view.addSubview(mainImageView)
+        view.addSubview(contentStack)
 
         NSLayoutConstraint.activate([
             mainImageView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 60.0),
             mainImageView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -60.0),
-            mainImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 140.0),
-            mainImageView.heightAnchor.constraint(equalTo: mainImageView.widthAnchor)
+            mainImageView.heightAnchor.constraint(equalTo: mainImageView.widthAnchor),
+
+            contentStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100.0),
+            contentStack.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20.0),
+            contentStack.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20.0),
+//            contentStack.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.6)
         ])
     }
 }
