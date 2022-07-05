5e4cce19f877300e1403d01764ebc3c9676a1500
Eldorbek <eldorbek.nusratov.98@gmail.com>
2022-07-05 20:56:23 +0500

    CreateApp

---
+++ PlantApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlantApp
+//
+//  Created by Eldorbek on 05/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
