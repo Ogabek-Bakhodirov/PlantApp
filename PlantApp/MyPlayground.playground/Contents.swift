cd719f718309a99dd9d6bc9cd8ef973d5eedaf11
elyahk <eldorbek.nusratov.98@gmail.com>
2022-07-16 21:46:46 +0500

    Remove unnecessary comments on `Playground.swift`

--- PlantApp/MyPlayground.playground/Contents.swift
+++ PlantApp/MyPlayground.playground/Contents.swift
@@ -1,18 +1,2 @@
 import UIKit
 
-var greeting = "Hello, playground"
-
-var arr = ["123", "232", "390", "3004", "3404", "343554"]
-
-
-let arrInt = arr.map { Int($0) }
-arrInt
-
-//var arrInt = [Int?]()
-//
-//arr.forEach { number in
-//    arrInt.append(Int(number))
-//}
-//
-//arrInt
-
